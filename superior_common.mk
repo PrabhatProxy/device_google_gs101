fea9f830f7873fce41e21e082d7f5fd4e13c5406
PrabhatProxy <prabhat@e.email>
2023-06-29 17:53:38 +0530

    gs101: Disable offending artifact checks

--- superior_common.mk
+++ superior_common.mk
@@ -7,6 +7,9 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-superior
 
+# Disable CSI checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay

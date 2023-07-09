e68de70ca96405daa9e486307dba1548eb81a0cf
PrabhatProxy <prabhat@e.email>
2023-07-09 23:18:41 +0530

    add files related to google camera

--- superior_common.mk
+++ superior_common.mk
@@ -10,6 +10,14 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-superior
 # Disable CSI checking
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay

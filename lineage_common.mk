24104421578a60602e46043a710c05fdb83744e8
Prabhat Maurya <prabhat@e.email>
2023-12-25 18:49:55 +0530

    Revert "gs101: Build Lineage Health HAL"
    This reverts commit 3b2dfd8eeed0737064013c3685b5b3d83f8c651e.

--- lineage_common.mk
+++ lineage_common.mk
@@ -22,10 +22,7 @@ PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
-
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
+    
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     device/google/gs101/linker.config.json
